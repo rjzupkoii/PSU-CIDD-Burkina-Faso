5494a4fbffb944611e5f774db8e5c1aca18f9dda
rjzupkoii <rjzii@yahoo.com>
2020-05-14 17:55:00 -0400

    Added epsilon concept

--- Analysis/PfPRMap/createBetaMap.py
+++ Analysis/PfPRMap/createBetaMap.py
@@ -4,8 +4,9 @@
 
 import csv
 
-# Margin used for PfPR values
-MARGIN = 0.001
+# Starting epsilon and delta to be used
+EPSILON = 0.00000001
+MAX_EPSILON = 1.0
 
 BETAVALUES = '../../Data/bf-population-beta.csv'
 PFPRVALUES = '../../GIS/bf_pfpr_raster.asc'
@@ -21,7 +22,6 @@ def load_betas():
             # Add a new entry for the population
             if not row['population'] in lookup:
                 lookup[row['population']] = []
-
 
             # Append the beta and PfPR
             lookup[row['population']].append( \
@@ -72,13 +72,35 @@ def write_asc(ascheader, ascdata, filename):
             ascfile.write('\n')
 
 
+# Get the beta values that generate the PfPR for the given popuation, this
+# function will start with the lowest epsilon value and increase it until
+# at least one value is found to be returned
+def get_betas(pfpr, population, lookup):
+    # Inital values
+    epsilon = 0
+    betas = []
+
+    # Increase the epsilon until at least one value is found
+    while len(betas) == 0:
+        epsilon += EPSILON
+        betas = get_betas_scan(pfpr, population, lookup, epsilon)
+
+        # Prevent an infinite loop, will result in an error
+        if epsilon == MAX_EPSILON:
+            print('Match not found!\nPfPR: ', pfpr, 'Population: ', population)
+            return [None, None]
+
+    # Return the results
+    return [betas, epsilon] 
+
+
 # Get the beta values that generate the PfPR for the given popuation, with the
 # given margin of error.
-def get_betas(pfpr, population, lookup, margin):
+def get_betas_scan(pfpr, population, lookup, epsilon):
 
     # Note the bounds
-    low = pfpr - margin
-    high = pfpr + margin
+    low = pfpr - epsilon
+    high = pfpr + epsilon
 
     # Scan the PfPR values for the popuation that are within the margin
     betas = []
@@ -109,6 +131,7 @@ def main():
     lookup = load_betas()
 
     # Prepare for the ASC data
+    epsilons = []
     minBeta = []
     meanBeta = []
     maxBeta = []
@@ -117,6 +140,7 @@ def main():
     for row in range(0, ascheader['nrows']):
 
         # Append the empty rows
+        epsilons.append([])
         minBeta.append([])
         meanBeta.append([])
         maxBeta.append([])
@@ -126,28 +150,37 @@ def main():
 
             # Append nodata and continue
             if pfpr[row][col] == ascheader['nodata']:
+                epsilons[row].append(ascheader['nodata'])
                 minBeta[row].append(ascheader['nodata'])
                 meanBeta[row].append(ascheader['nodata'])
                 maxBeta[row].append(ascheader['nodata'])
                 continue
 
             # Get the beta values
-            values = get_betas(pfpr[row][col], \
-                               population[row][col], lookup, MARGIN)
+            [values, epsilon] = get_betas( \
+                pfpr[row][col], population[row][col], lookup)
 
             # Was nothing returned?
             if len(values) == 0:
+                epsilons[row].append(0)
                 minBeta[row].append(0)
                 meanBeta[row].append(0)
                 maxBeta[row].append(0)
                 continue
 
+            # Note the epsilon
+            epsilons[row].append(epsilon)
+
             # Get the min, mean, and max
             minBeta[row].append(min(values))
             meanBeta[row].append(sum(values) / len(values))
             maxBeta[row].append(max(values))
+
+            # Note the progress
+            print(row, col)
             
     # Write the results
+    write_asc(ascheader, epsilons, 'bf_epsilons_beta.asc')
     write_asc(ascheader, minBeta, 'bf_min_beta.asc')
     write_asc(ascheader, meanBeta, 'bf_mean_beta.asc')
     write_asc(ascheader, maxBeta, 'bf_max_beta.asc')
