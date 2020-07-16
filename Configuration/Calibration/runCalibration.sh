6e52bf4a81ed8ec8fb938181524599ab0a69353e
rjzupkoii <rjzii@yahoo.com>
2020-07-16 19:23:19 -0400

    Updated calibration scripts

--- Configuration/Calibration/runCalibration.sh
+++ Configuration/Calibration/runCalibration.sh
@@ -1,32 +1,43 @@
 #!/bin/bash
 
+# This script will queue processes on the cluster up to the configured limit. The filenames
+# will encode the values based using the name POPUATION-ACCESS-BETA-bfa.yml although a fixed
+# study id is in place as well.
+
 # The number of processes to run
 LIMIT=40
 
-# Run using ten natural breaks
+# Iterate over the natural breaks in the popuation
 for population in 797 1417 2279 3668 6386 12627 25584 53601 117418; do
 
   # Prepare the ASC file
   sed 's/#POPULATION#/'"$population"'/g' population.asc > $population.asc
 
-  for beta in `seq 0.022 0.001 1.185`; do
-    # Get the current job count, note the overcount due to the delay.
-    # Wait if there are currently too many jobs
-    while [ `qstat -u rbz5100 | grep rbz5100 | wc -l` -gt $LIMIT ]
-    do
-      sleep 10s
+  # Iterate off the possible levels of access
+  for access in 0.5 0.65 0.7 0.75 0.8 0.85 0.9; do
+
+    # Itterate over the possible beta values
+    for beta in `seq 0.02 0.005 1.185`; do
+      # Get the current job count, note the overcount due to the delay.
+      # Wait if there are currently too many jobs
+      while [ `qstat -u rbz5100 | grep rbz5100 | wc -l` -gt $LIMIT ]
+      do
+        sleep 10s
+      done
+
+      # Prepare the configuration file
+      sed 's/#BETA#/'"$beta"'/g' bf-calibration.yml > $population-$access-$beta-bfa.yml
+      sed -i 's/#POPULATION#/'"$population"'/g' $population-$access-$beta-bfa.yml  
+      sed -i 's/#ACCESS#/'"$access"'/g' $population-$access-$beta-bfa.yml
+  
+      sed 's/#BETA#/'"$beta"'/g' template.job > $population-$access-$beta-bfa.pbs
+      sed -i 's/#POPULATION#/'"$population"'/g' $population-$access-$beta-bfa.pbs
+      sed -i 's/#ACCESS'"$access"'/g' $population-$access-$beta-bfa.pbs
+  
+      # Queue the next item
+      qsub $population-$access-$beta-bfa.pbs
+
     done
-
-    # Prepare the configuration file
-    sed 's/#BETA#/'"$beta"'/g' bf-calibration.yml > bf-$population-$beta.yml
-    sed -i 's/#POPULATION#/'"$population"'/g' bf-$population-$beta.yml  
- 
-   sed 's/#BETA#/'"$beta"'/g' template.job > bf-$population-$beta.pbs
-   sed -i 's/#POPULATION#/'"$population"'/g' bf-$population-$beta.pbs  
- 
-    # Queue the next item
-    qsub bf-$population-$beta.pbs
-
   done
 done
 
