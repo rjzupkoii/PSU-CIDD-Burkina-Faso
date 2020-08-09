2bc5483fe68b16f83133a67af86c759c97b0f5eb
rjzupkoii <rjzii@yahoo.com>
2020-08-08 20:42:21 -0400

    Updated scripts

--- Analysis/Population/population_analysis.m
+++ Analysis/Population/population_analysis.m
@@ -3,28 +3,54 @@
 % Generate plots that allow us to compare how the model performs at scale.
 clear;
 
-global raw;
+global raw reference;
 raw = csvread('data/population-full.csv', 1, 0);
+reference = csvread('data/weighted_pfpr.csv');
 
-%plot_population();
-plot_error();
+plot_population();
+%plot_error();
+%plot_error_summary();
 
-function [] = plot_error() 
-    global raw;
-    
-    dayselapsed = unique(raw(:, 1));
-    districts = unique(raw(:, 2));
+function [] = plot_error_summary() 
+    global raw reference;
 
-    reference = csvread('data/weighted_pfpr.csv');
-
-    colors = colormap(jet(size(districts, 1)));
+    % Trim off all of the data prior to burn-in
+    data = raw(raw(:, 1) >= 4000, :);
+    districts = unique(data(:, 2));
 
     hold on;
     for district = transpose(districts)
         expected = reference(reference(:, 1) == district, 2);
+        pfpr = data(data(:, 2) == district, 6);
+        values = (pfpr - expected) / expected;
+        variance = var(values) * 100;
+        error = sum(values) / size(values, 1) * 100;
+        scatter(error, variance, 'filled');
+        text(error + 0.025, variance + 0.0005, cellstr(num2str(district)));
+    end
+
+    title('Population Scaling versus Expected Values (Post-Burn-in)');
+    xlabel('Mean Percent Error');
+    ylabel('Error Variance');
+
+    graphic = gca;
+    graphic.FontSize = 18;
+    hold off;
+end
+
+function [] = plot_error() 
+    global raw reference;
+    
+    dayselapsed = unique(raw(:, 1));
+    districts = unique(raw(:, 2));
+
+    hold on;
+    colors = colormap(parula(size(districts, 1)));
+    for district = transpose(districts)
+        expected = reference(reference(:, 1) == district, 2);
         pfpr = raw(raw(:, 2) == district, 6);
         error = ((pfpr - expected) / expected) * 100;
-        scatter(dayselapsed, error, 30, colors(district, :), 'Filled');    
+        scatter(dayselapsed, error, 50, colors(district, :), 'Filled');    
     end 
 
     yline(0, '-.');
@@ -34,6 +60,8 @@ function [] = plot_error()
     ylabel('Percent Error');
     xlabel('Days Elapsed');
 
+    graphic = gca;
+    graphic.FontSize = 18;
     hold off;
 end
 
@@ -58,4 +86,6 @@ function [] = plot_population()
     title('Growth of Simulated Population');
     ylabel('Population (millions)');
     xlabel('Days Elapsed');
+    graphic = gca;
+    graphic.FontSize = 18;    
 end
--- Configuration/rainy_season.m
+++ Configuration/rainy_season.m
@@ -1,32 +1,43 @@
-% Orginal equation
-% y = a - min * sin((2 * pi / period) * t + (-phi * (2 * pi / period))))
-
-% Simplified equation
-% y = a + min * sin((2 * pi * (phi - t)) / period);
-
-clear all;
+% rainy_season.m
+%
+% This file contains settings and plots that can be used to fit the
+% seasonality for a given location. The seasonality works off the equation:
+%   
+%   multiplier = base + (a * sin?(b * pi * (phi - t) / 365))
+%
+% where base is the minimum multiplier to apply.
+clear;
 
 hold on;
 
+% MMC = 1 + sin(2 * pi * t / 365)
+%[mmc, mmc_rain] = seasonality(1, 1, 2, 0);
+%plot(1:365, mmc);
+
 % Reference settings for no seasonality adjustment (i.e., persistant)
-%[flat, flat_rain] = seasonality(0, 0, 1, 1, 1);
+%[flat, flat_rain] = seasonality(1, 0, 1, 0);
 %plot(1:365, flat);
 
-[short, short_rain] = seasonality(-0.9, -1.8, 140, 320, 0.1);
+[short, short_rain] = seasonality(0.1, -0.9, 2.5, 146);
 item(1) = plot(1:365, short);
-item(2) = plot(1:365, short_rain);
 
-[long, long_rain] = seasonality(-0.9, -1.8, 132, 415, 0.1);
-item(3) = plot(1:365, long);
-item(4) = plot(1:365, long_rain);
+[long, long_rain] = seasonality(0.1, -0.9, 2.3, 155);
+item(2) = plot(1:365, long);
 
-item(5) = xline(166, '-.', 'Approximate Start of Rainy Season', 'FontSize', 18);
-item(6) = xline(274, '-.', 'Approximate End of Short Rainy Season', 'FontSize', 18);
-item(7) = xline(305, '-.', 'Approximate End of Long Rainy Season', 'FontSize', 18);
+%plot(1:365, short_rain);
+%plot(1:365, long_rain);
 
-legend(item([1 3]), 'Short Seasonality', 'Long Seasonality', 'FontSize', 18);
+xline(220, '-.', 'Midpoint Short Rainy Season', 'FontSize', 18);
+xline(235, '-.', 'Midpoint Long Rainy Season', 'FontSize', 18);
+
+xline(166, '-.', 'Approximate Start of Rainy Season', 'FontSize', 18);
+xline(274, '-.', 'Approximate End of Short Rainy Season', 'FontSize', 18);
+xline(305, '-.', 'Approximate End of Long Rainy Season', 'FontSize', 18);
 
 title('Seasonality of Malaira in Burkina Faso', 'fontsize', 32);
+legend(item, 'Short Seasonality', 'Long Seasonality', 'FontSize', 18, 'Location','northwest');
+legend('boxoff')
+
 xlabel('Month', 'fontsize', 24);
 ylabel('Beta Multiplier', 'fontsize', 24);
 datetick('x', 'mmm');
@@ -34,9 +45,9 @@ datetick('x', 'mmm');
 plot = gca;
 plot.FontSize = 18;
 
-function [multiplier, rainy] = seasonality(a, b, phi, period, base)
+function [multiplier, rainy] = seasonality(base, a, b, phi)
     t = 1:365;
-    multiplier = a + b * sin((2 * pi * (phi - t)) / period);
+    multiplier = a * sin(b * pi * (phi - t) / 365);
     multiplier(multiplier < 0) = 0;
     multiplier = base + multiplier;
     rainy = (multiplier > base);
