0dba6f866f60f77e6d3985cb98fccf10e657c1eb
rjzupkoii <rjzii@yahoo.com>
2020-08-10 17:48:00 -0400

    Updated calibration query

--- Configuration/Calibration/betas_query.sql
+++ Configuration/Calibration/betas_query.sql
@@ -1,8 +1,9 @@
 SELECT replicateid,
   filename,
-  cast((regexp_matches(filename, '^(\d*)-([\.\d]*)-([\.\d]*)'))[1] as integer) AS population,
-  cast((regexp_matches(filename, '^(\d*)-([\.\d]*)-([\.\d]*)'))[2] as float) AS access,
-  cast((regexp_matches(filename, '^(\d*)-([\.\d]*)-([\.\d]*)'))[3] as float) AS beta,
+  cast((regexp_matches(filename, '^(\d*)-(\d*)-([\.\d]*)-([\.\d]*)'))[1] as integer) AS zone,
+  cast((regexp_matches(filename, '^(\d*)-(\d*)-([\.\d]*)-([\.\d]*)'))[2] as integer) AS population,
+  cast((regexp_matches(filename, '^(\d*)-(\d*)-([\.\d]*)-([\.\d]*)'))[3] as float) AS access,
+  cast((regexp_matches(filename, '^(\d*)-(\d*)-([\.\d]*)-([\.\d]*)'))[4] as float) AS beta,
   avg(eir) AS eir, 
   avg(pfprunder5) AS pfprunder5, 
   avg(pfpr2to10) AS pfpr2to10, 
@@ -11,7 +12,7 @@ FROM sim.configuration c
   INNER JOIN sim.replicate r on r.configurationid = c.id
   INNER JOIN sim.monthlydata md on md.replicateid = r.id
   INNER JOIN sim.monthlysitedata msd on msd.monthlydataid = md.id
-WHERE studyid = 6
+WHERE studyid = 8
   AND md.dayselapsed >= 3987
 GROUP BY replicateid, filename
-ORDER BY population, access, pfpr2to10
+ORDER BY zone, population, access, pfpr2to10
