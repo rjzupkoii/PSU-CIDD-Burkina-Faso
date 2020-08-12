b74d9727badf4ebc92c4269a3c076eb49f159d11
rjzupkoii <rjzii@yahoo.com>
2020-08-12 16:05:25 -0400

    Updates to missing script

--- Analysis/PfPRMap/findMissing.py
+++ Analysis/PfPRMap/findMissing.py
@@ -4,26 +4,23 @@
 
 import csv
 
-BETAVALUES = 'data/calibration.csv'
+#BETAVALUES = 'data/calibration.csv'
+BETAVALUES = 'data/working.csv'
+
 RESULTS = 'out/missing.csv'
 
+# Prepare the variables
+population = set()
+treatment = set()
+beta = set()
+missing = []
+raw = []
 
-def main():
-    # Prepare our data structures
-    population = set()
-    treatment = set()
-    beta = set()
-    raw = []
+def check(zone):
+    global missing, population, treatment, beta
 
-    # Start by reading the raw population, treatment rate, and beta
-    with open(BETAVALUES) as csvfile:
-        reader = csv.DictReader(csvfile)
-        raw = []
-        for row in reader:
-            population.add(row['population'])
-            treatment.add(row['access'])
-            beta.add(row['beta'])
-            raw.append([row['population'], row['access'], row['beta']])
+    # Check-in with user
+    print "Checking zone {}".format(zone)
 
     # Unique values are loaded, sort them
     population = sorted(population, reverse=True)
@@ -34,17 +31,49 @@ def main():
     print "{} combinations expected".format(len(population) * len(treatment) * len(beta))
 
     # Scan for the matching row, add it to the missing list if not found
-    missing = []
     for ndx in population:
         for ndy in treatment:
             for ndz in beta:
-                row = [ndx, ndy, ndz]
+                row = [ ndx, ndy, ndz]
                 matched = False
                 for index in range(0, len(raw) - 1):
                     if row == raw[index]:
                         matched = True
                 if not matched:
-                    missing.append(row)
+                    missing.append([zone, ndx, ndy, ndz])
+
+
+def main():
+    global missing, raw, population, treatment, beta
+
+    # We aren't in a zone yet
+    zone = None
+
+    # Start by reading the raw population, treatment rate, and beta
+    with open(BETAVALUES) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            # Set the zone if not set
+            if zone is None:
+                zone = row['zone']
+
+            # Are we in a new block?
+            if zone != row['zone']:
+                check(zone)
+                raw = []
+                population = set()
+                treatment = set()
+                beta = set()
+                zone = row['zone']
+
+            # Set the values
+            population.add(row['population'])
+            treatment.add(row['access'])
+            beta.add(row['beta'])
+            raw.append([row['population'], row['access'], row['beta']])
+
+    # Check the last zone
+    check(zone)
     
     # Print the number missing
     print "{} combinations missing".format(len(missing))
