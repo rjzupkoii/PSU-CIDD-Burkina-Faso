e0fdc9db9c025f491f775cff28f2df709e5dbc4a
rjzupkoii <rjzii@yahoo.com>
2021-02-04 22:49:07 -0500

    Plot mostly done

--- Plots/treatment_failures.m
+++ Plots/treatment_failures.m
@@ -5,48 +5,111 @@ addpath('../Analysis/Common');
 addpath('include');
 clear;
 
+DIRECTORY = '../Analysis/Loader/out';
 STARTDATE = '2007-1-1';
 
-raw = csvread('data/treatment-failures.csv', 1, 0);
-dn = transpose(prepareDates('data/treatment-failures.csv', 3, STARTDATE));
+plot_treatment_failures(DIRECTORY, STARTDATE);
 
-mapping = readtable('data/id-mapping.csv');
-colors = colormap(lines(size(mapping, 1) + 1));
+function [] = plot_treatment_failures(directory, startDate)
 
-labels = {};
+    % Prepare a color map to use by counting the number of directories
+    % NOTE this seems to allocate 2x as many as are needed for some reason
+    folders = dir(directory);
+    folders(ismember( {folders.name}, {'.', '..'})) = [];
 
-hold on;
-for row = 1:size(mapping)
-    id = table2array(mapping(row, 1));
-    for replicate = unique(raw(raw(:, 1) == id, 2))'
-        data = raw(raw(:, 1) == id, :);
-        data = data(data(:, 2) == replicate, :);
-        plot(dn, data(:, 4), 'Color', colors(row, :));
+    % Prepare a color map
+    colors = colormap(lines(size(folders, 1) / 2));
+    ci = 1;    
+
+    % Append the files to the plot
+    top = {}; bottom = {};
+    files = dir(directory);
+    for ndx = 1:length(files)
+        % Skip anything that is not the directories we are looking for
+        if ~files(ndx).isdir, continue; end
+        if strcmp(files(ndx).name(1), '.'), continue; end
+                
+        % Prepare to append to the plot
+        filename = fullfile(files(ndx).folder, files(ndx).name);
+        label = strrep(parse_name(files(ndx).name), 'with ', '');
+        if startsWith(label, '0.0')
+            location = 1;
+            top = [top, label];    
+        else
+            location = 2;
+            bottom = [bottom, label];
+        end
+        
+        % Update the plot
+        hold on;
+        subplot(2, 1, location);
+        append_plots(filename, colors(ci, :));
+        ci = ci + 1;
     end
+        
+    % Format the plots
+    subplot(2, 1, 1);
+    format_legend(top, colors);
+    format_plot('Comparison of Muation Rates', startDate);
     
-    % Parse out the name to use for the legend
-    name = parse_name(string(strrep(table2cell(mapping(row, 2)), '.yml', '')));
-    name = strrep(name, 'with ', '');
-    labels = [labels, name];
+    subplot(2, 1, 2);
+    format_legend(bottom, colors);
+    format_plot('Comparison of Possible Interventions', startDate);    
 end
-add_legend(labels, colors);
-hold off;
 
-title('Comparison of Possible Interventions');
-ylabel('Rate of Treatment Failures (population adjusted)');
-datetick('x', 'yyyy');
-xlabel('Model Year');
 
-graphic = gca;
-graphic.FontSize = 24;
+function [] = append_plots(directory, color)
+    files = dir(fullfile(directory, '*treatment*.csv'));
+    for ndx = 1:length(files)
+        % Load the data, note the unique days
+        filename = fullfile(files(ndx).folder, files(ndx).name);
+        data = csvread(filename, 1, 0);
+        data = data(data(:, 2) > 13 * 365, :);
+        
+        % Total each day
+        days = transpose(unique(data(:, 2)));
+        total = zeros(size(days, 1)); index = 1;
+        for day = days
+            total(index) = sum(data(data(:, 2) == day, 6));
+            index = index + 1;
+        end
+        
+        % Append the plot
+        plot(days, log10(total), 'Color', color);
+    end
+end
 
-function [] = add_legend(labels, colors)
-    h = zeros(1, 1);
+
+function [] = format_legend(labels, colors)
+    % Add and format the legend
+    item = zeros(1, 1);
     index = 1;
     for label = labels
-        h(index) = scatter(NaN, NaN, [], colors(index, :), 'filled');
+        item(index) = scatter(NaN, NaN, [], colors(index, :), 'filled');
         index = index + 1;
     end
-	legend(h, cellstr(labels'), 'Location', 'NorthEast');
+	legend(item, labels, 'Location', 'NorthWest', 'FontSize', 16);
     legend boxoff;
+end
+
+
+function [] = format_plot(plotTitle, startDate)    
+    % Format the rest of the plot
+    title(plotTitle);
+    ylabel('Treatment Failures (log_{10})');    
+    xlabel('Model Year');
+    axis tight;
+    
+    % Format the dates based upon the xaxis and ticks, this code might
+    % break if the bounds change
+    labels = [];
+    for tick = xticks()
+        tick = addtodate(datenum(startDate), tick, 'day');
+        labels = [labels str2num(datestr(tick, 'yyyy'))];
+    end
+    set(gca, 'XTick', xticks(), 'XTickLabel', labels)
+        
+    % Set the font sizes
+    graphic = gca;
+    graphic.FontSize = 24;
 end
