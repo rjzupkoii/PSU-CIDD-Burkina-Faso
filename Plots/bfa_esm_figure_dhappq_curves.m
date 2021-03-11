8cf51fa74ccc553b472728b12a8e3316750fd55a
rjzupkoii <rjzii@yahoo.com>
2021-03-10 20:39:01 -0500

    Updated with long run data

--- Plots/bfa_esm_figure_dhappq_curves.m
+++ Plots/bfa_esm_figure_dhappq_curves.m
@@ -13,21 +13,11 @@ add_data('DHA-PPQ_data/DP_0.100/_aggregate_data_0.csv');
 add_data('DHA-PPQ_data/DP_0.150/_aggregate_data_0.csv');
 add_data('DHA-PPQ_data/DP_0.200/_aggregate_data_0.csv');
 hold off;
-
-ylabel('Frequency');
-xlabel('Model Year');
-datetick('x', 'yyyy')
-
-legend({'0.051 (Burkina Faso baseline)', '0.100', '0.150', '0.200'}, 'Location', 'NorthWest');
-legend boxoff;
-title('Plasmepsin Double Copy Frequency');
-axis tight;
-
-graphic = gca;
-graphic.FontSize = 18;
+format_plot('Plasmepsin Double Copy Frequency, Usage Comparison', {'0.051 (Burkina Faso baseline)', '0.100', '0.150', '0.200'});
 
 subplot(2, 1, 2);
-
+add_data('DHA-PPQ_data/LongRun/_aggregate_data_0.csv');
+format_plot('Plasmepsin Double Copy Frequency, Long Run', '0.051 (Burkina Faso baseline)');
 
 function [] = add_data(filename)
     % Read and discard the first 4000 days of data (burn-in)
@@ -40,4 +30,18 @@ function [] = add_data(filename)
         dates(ndx) = addtodate(datenum('2007-1-1'), dates(ndx), 'day');
     end
     plot(dates, data(:, 11) ./ data(:, 3));  % Plasmepsin2xCopyWeighted
+end
+
+function [] = format_plot(plotTitle, legendItems)
+    ylabel('Frequency');
+    xlabel('Model Year');
+    datetick('x', 'yyyy')
+
+    legend(legendItems, 'Location', 'NorthWest');
+    legend boxoff;
+    title(plotTitle);
+    axis tight;
+
+    graphic = gca;
+    graphic.FontSize = 18;
 end
