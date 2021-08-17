cd2cd098ee6a7aea85337ff05f74d7ff04f71e33
rjzupkoii <rjzii@yahoo.com>
2021-08-16 22:04:17 -0400

    Additional analysis

--- Analysis/Importation/importation.m
+++ Analysis/Importation/importation.m
@@ -4,21 +4,26 @@
 % fixation following importation.
 clear;
 
-for imports = 3:3:9
-    for symptomatic = 0:1
-        for mutations = 0:1
-            % filename, symptomatic, mutations, imports
-            plot_importation('data/data.csv', symptomatic, mutations, imports);
-            
-            % Save the image to disk
-            set(gcf, 'Position',  [0, 0, 2560, 1440]);
-            image = sprintf('out/import-%d-symptomatic-%d-mutations-%d.png', ... 
-                imports, symptomatic, mutations);
-            print('-dtiff', '-r300', image);
-            clf;
-            close;               
+% All 580Y genotypes
+generate('data/data.csv', 'out/allgenotypes-%d-symptomatic-%d-mutations-%d.png');
+
+% TNY--Y1x genotypes only
+generate('data/data-import.csv', 'out/tny--y1x-%d-symptomatic-%d-mutations-%d.png');
+
+function [] = generate(filename, imagename)
+    for imports = 3:3:9
+        for symptomatic = 0:1
+            for mutations = 0:1
+                % filename, symptomatic, mutations, imports
+                plot_importation(filename, symptomatic, mutations, imports);
+
+                % Save the image to disk
+                set(gcf, 'Position',  [0, 0, 2560, 1440]);
+                image = sprintf(imagename, imports, symptomatic, mutations);
+                print('-dtiff', '-r300', image);
+                clf;
+                close;               
+            end
         end
     end
 end
-
-
