d08185c9ede56c7b1b5c5a3388e00134d12e9aed
rjzupkoii <rjzii@yahoo.com>
2021-10-11 11:42:08 -0400

    MV generation script

--- Plots/analysis/generate_bfa_plots.m
+++ Plots/analysis/generate_bfa_plots.m
@@ -16,12 +16,12 @@ if ~exist('out', 'dir'), mkdir('out'); end
 %      exact sizes can be changed though
 plot_heatmaps(FREQUENCY, STARTDATE);
 
-% plot_district_frequency(DIRECTORY, STARTDATE);
-% plot_regional_frequency(DIRECTORY, STARTDATE);
-% plot_national_frequency(DIRECTORY, STARTDATE);
-%  
-% plot_frequency_treatmentfailures(DIRECTORY, STARTDATE);
-% plot_genotype_frequency(DIRECTORY, STARTDATE);
-% 
-% plot_treatment_data(DIRECTORY, STARTDATE);
-% plot_treatment_failures(DIRECTORY, STARTDATE);
+plot_district_frequency(DIRECTORY, STARTDATE);
+plot_regional_frequency(DIRECTORY, STARTDATE);
+plot_national_frequency(DIRECTORY, STARTDATE);
+ 
+plot_frequency_treatmentfailures(DIRECTORY, STARTDATE);
+plot_genotype_frequency(DIRECTORY, STARTDATE);
+
+plot_treatment_data(DIRECTORY, STARTDATE);
+plot_treatment_failures(DIRECTORY, STARTDATE);
