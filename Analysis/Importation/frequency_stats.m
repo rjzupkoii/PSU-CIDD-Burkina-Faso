088926522fad516eb9643429188a495c7479e3b3
rjzupkoii <rjzii@yahoo.com>
2021-12-03 14:19:43 -0500

    More functional

--- Analysis/Importation/frequency_stats.m
+++ Analysis/Importation/frequency_stats.m
@@ -5,35 +5,43 @@
 addpath('include');
 clear;
 
-FILENAME = 'data/bfa-merged.csv';
+process('data/bfa-merged.csv', true);
 
-% Load the data and drop de novo studies
-data = readtable(FILENAME, 'PreserveVariableNames', true);
-data = data(data.mutations == 0, :);
-
-% Restrict the data to the last recorded date
-dates = unique(data.dayselapsed);
-data = data(data.dayselapsed == max(dates), :);
-
-% Calcluate the frequency and store as log10
-data.frequency = log10(data.weightedoccurrences ./ data.infectedindividuals);
-
-% Generate the counts
-for imports = 3:3:9
-    for symptomatic = 0:1
-        % First pass filtering
-        filtered = data(data.imports == imports, :);
-        filtered = filtered(filtered.symptomatic == symptomatic, :);
-        
-        % Filter to the month prepare the block data
-        block = zeros(50, 12);
-        for month = 1:12
-            monthly = filtered(filtered.month == month, :);
-            frequency = monthly.frequency;
-            frequency(length(frequency) + 1:50) = NaN;
-            block(:, month) = frequency;
+function [] = process(filename, plot)
+    % Load the data and drop de novo studies
+    data = readtable(filename, 'PreserveVariableNames', true);
+    data = data(data.mutations == 0, :);
+    
+    % Restrict the data to the last recorded date
+    dates = unique(data.dayselapsed);
+    data = data(data.dayselapsed == max(dates), :);
+    
+    % Calcluate the frequency and store as log10
+    data.frequency = log10(data.weightedoccurrences ./ data.infectedindividuals);
+    
+    % Generate the counts
+    for imports = 3:3:9
+        for symptomatic = 0:1
+            % First pass filtering
+            filtered = data(data.imports == imports, :);
+            filtered = filtered(filtered.symptomatic == symptomatic, :);
+            
+            % Filter to the month prepare the block data
+            block = zeros(50, 12);
+            for month = 1:12
+                monthly = filtered(filtered.month == month, :);
+                frequency = monthly.frequency;
+                frequency(length(frequency) + 1:50) = NaN;
+                block(:, month) = frequency;
+            end
+%            block(block == -Inf) = NaN;        % TODO Determine if this is valid
+    
+            % Save the data for the figure to disk
+            file = sprintf('out/final-frequency-%d-symptomatic-%d.csv', imports, symptomatic);
+            writematrix(block, file);
+    
+            if plot; generate_plot(block, imports, symptomatic); end
         end
-        generate_plot(block, imports, symptomatic);
     end
 end
 
@@ -52,8 +60,4 @@ function [] = generate_plot(block, imports, symptomatic)
     print('-dtiff', '-r300', image);
     clf;
     close;
-
-    % Save the data for the figure to disk
-    file = sprintf('out/final-frequency-%d-symptomatic-%d.csv', imports, symptomatic);
-    writematrix(block, file);
 end
