c3424632d758fe82002a0e57b5cfaa286fb58abe
rjzupkoii <rjzii@yahoo.com>
2022-02-07 16:53:42 -0500

    Finalizing plot selection

--- Analysis/Importation/plot_importation.py
+++ Analysis/Importation/plot_importation.py
@@ -19,30 +19,30 @@ CLINICAL_PROBABILITY, MIDPOINT, IMMUNE_EFFECT = 0.99, 0.15, 4.5
 
 COLUMN, PARTNER, YLABEL = 0, 1, 2  
 REPORT_LAYOUT = {
-    'clinical' : ['ClinicalIndividuals', None, 'Clinical Cases, per 1000'],
-    'clones' : ['Multiclonal', None, 'Multiclonal, per 1000'],
-    'failures' : ['TreatmentFailure', None, 'Failures, per 1000'],    
-    'frequency' : ['MARKER', 'theta', '580Y Frequency'],
-    'infections' : ['InfectedIndividuals', None, 'Infections, per 1000'],
-    'newinfections' : ['NewInfections', None, 'New Infections, per 1000'],
-    'phi' : ['MARKER', None, 'Phi'],
-    'symptoms' : ['MARKER', None, 'Probability of Symptoms'],
+    'clones' : ['Multiclonal', None, 'Proportion Multiclonal'],
+    'failures' : ['TreatmentFailure', None, 'Treatment Failures'], 
     'theta' : ['Theta', None, 'Theta'],
-    'treatments' : ['Treatments', None, 'Treatments, per 1000'],
-    'unweighted' : ['508yUnweighted', None, '580Y Clone Count'],
-
-    # 'clinical-phi' : ['ClinicalIndividuals', 'treatments', 'Clinical Cases, per 1000'],
     'clinical-theta' : ['ClinicalIndividuals', 'theta', 'Clinical Cases, per 1000'],
     'clinical-failures' : ['ClinicalIndividuals', 'failures', 'Clinical Cases, per 1000'],
-    # 'failures-phi' : ['TreatmentFailure', 'phi', 'Failures, per 1000'],
     'infections-failures' : ['InfectedIndividuals', 'failures', 'Infections, per 1000'],
+    'treatments-theta' : ['Treatments', 'theta', 'Treatments'],
+    'unweighted-clones' : ['508yUnweighted', 'clones', '580Y Clone Count'],
+    'unweighted-failures' : ['508yUnweighted', 'failures', '580Y Clone Count'],
+
+    # 'clinical' : ['ClinicalIndividuals', None, 'Clinical Cases, per 1000'],
+    # 'frequency' : ['MARKER', 'theta', '580Y Frequency'],
+    # 'infections' : ['InfectedIndividuals', None, 'Infections, per 1000'],
+    # 'newinfections' : ['NewInfections', None, 'New Infections, per 1000'],
+    # 'phi' : ['MARKER', None, 'Phi'],
+    # 'symptoms' : ['MARKER', None, 'Probability of Symptoms'],
+    # 'treatments' : ['Treatments', None, 'Treatments'],
+    # 'unweighted' : ['508yUnweighted', None, '580Y Clone Count'],
+    # 'clinical-phi' : ['ClinicalIndividuals', 'treatments', 'Clinical Cases, per 1000'],
+    # 'failures-phi' : ['TreatmentFailure', 'phi', 'Failures, per 1000'],
     # 'infections-clones' : ['InfectedIndividuals', 'clones', 'Infections, per 1000'],
     # 'symptoms-failures' : ['MARKER', 'failures', 'Probability of Symptoms'],
     # 'symptoms-phi' : ['MARKER', 'phi', 'Probability of Symptoms'],
     # 'symptoms-treatments' : ['MARKER', 'treatments', 'Probability of Symptoms'],
-    'treatments-theta' : ['Treatments', 'theta', 'Treatments, per 1000'],
-    'unweighted-clones' : ['508yUnweighted', 'clones', '580Y Clone Count'],
-    'unweighted-failures' : ['508yUnweighted', 'failures', '580Y Clone Count'],
     # 'unweighted-infections' : ['508yUnweighted', 'infections', '580Y Clone Count'],
     # 'unweighted-newinfections' : ['508yUnweighted', 'newinfections', '580Y Clone Count'],
     # 'unweighted-phi' : ['508yUnweighted', 'phi', '580Y Clone Count'],
@@ -95,22 +95,16 @@ def prepare(path):
                 if key.startswith('clinical'):
                     row = byZone['ClinicalIndividuals'] / (byZone['Population'] / 1000.0)
                 elif key.startswith('clones'):
-                    row = byZone['Multiclonal'] / (byZone['InfectedIndividuals'] / 1000.0)
-                elif key.startswith('failures'):
-                    row = byZone['TreatmentFailure'] / (byZone['Treatments'] / 1000.0)
+                    row = byZone['Multiclonal'] / byZone['InfectedIndividuals']
                 elif key.startswith('frequency'):
                     row = byZone['580yWeighted'] / byZone['InfectedIndividuals']
                 elif key.startswith('infections'):
                     row = byZone['InfectedIndividuals'] / (byZone['Population'] / 1000.0)
-                elif key.startswith('newinfections'):
-                    row = byZone['NewInfections'] / (byZone['Population'] / 1000.0)                    
                 elif key.startswith('phi'):
                     row = byZone['ClinicalIndividuals'] / byZone['InfectedIndividuals']
                 elif key.startswith('symptoms'):
                     # isf.max_clinical_probability / (1 + pow((immune / isf.midpoint), isf.immune_effect_on_progression_to_clinical)
                     row = CLINICAL_PROBABILITY / (1 + pow((byZone['Theta'] / MIDPOINT), IMMUNE_EFFECT))
-                elif key.startswith('treatments'):
-                    row = byZone['Treatments'] / (byZone['InfectedIndividuals'] / 1000.0)
                 else:        
                     row = byZone[REPORT_LAYOUT[key][COLUMN]].to_numpy()
                     
@@ -138,27 +132,26 @@ def report(dates, data, title, out):
 
     count = 0
     for key in REPORT_LAYOUT:
-        if '-' in key:
-            figure, axes = plt.subplots(3, 1)
-            for zone in range(3):
-                # Add the base plot       
-                add_plot(axes[zone], dates, data[zone][key], REPORT_LAYOUT[key][YLABEL], '#0571b0')
+        figure, axes = plt.subplots(3, 1)
+        for zone in range(3):
+            # Add the base plot       
+            add_plot(axes[zone], dates, data[zone][key], REPORT_LAYOUT[key][YLABEL], '#0571b0')
 
-                # Add the partner plot if one is provided
-                if REPORT_LAYOUT[key][PARTNER] != None:
-                    partner = REPORT_LAYOUT[key][PARTNER]
-                    add_plot(axes[zone].twinx(), dates, data[zone][partner], REPORT_LAYOUT[partner][YLABEL], '#ca0020')
+            # Add the partner plot if one is provided
+            if REPORT_LAYOUT[key][PARTNER] != None:
+                partner = REPORT_LAYOUT[key][PARTNER]
+                add_plot(axes[zone].twinx(), dates, data[zone][partner], REPORT_LAYOUT[partner][YLABEL], '#ca0020')
 
-                # Set the subplot title
-                axes[zone].title.set_text(ZONES[zone])
+            # Set the subplot title
+            axes[zone].title.set_text(ZONES[zone])
 
-            # Format the overall plot
-            figure.suptitle(title)
+        # Format the overall plot
+        figure.suptitle(title)
 
-            # Save the report
-            os.makedirs('plots', exist_ok=True)
-            plt.savefig('plots/{}-{}.png'.format(out, key))
-            plt.close()
+        # Save the report
+        os.makedirs('plots', exist_ok=True)
+        plt.savefig('plots/{}-{}.png'.format(out, key))
+        plt.close()
 
         # Update the progress bar
         count += 1
