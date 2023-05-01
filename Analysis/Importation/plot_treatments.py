5685837a2e6c889377500abd89207a29c8d06167
rjzupkoii <rjzii@yahoo.com>
2023-05-01 16:07:53 -0400

    Four panel comparison of treatment seeking

--- Analysis/Importation/plot_treatments.py
+++ Analysis/Importation/plot_treatments.py
@@ -10,6 +10,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import re
 import sys
 
 # From the PSU-CIDD-MaSim-Support repository
@@ -34,9 +35,9 @@ def plot_data(data, dates, ax):
     lower = np.percentile(dataset, 25, axis=0)            
     
     # Add the data to the subplot
-    ax.plot(dates, median)
+    ax.plot(dates, median, zorder=100)
     color = scale_luminosity(ax.lines[-1].get_color(), 1)
-    ax.fill_between(dates, lower, upper, alpha=0.5, facecolor=color)    
+    ax.fill_between(dates, lower, upper, alpha=0.5, facecolor=color, zorder=100)    
     
 
 matplotlib.rc_file('include/matplotlibrc-line')
@@ -51,8 +52,9 @@ dates = [startDate + datetime.timedelta(days = int(x)) for x in dates]
 
 
 figure, plots = plt.subplots(2, 2)
-row, col = 0, 0
+figure.suptitle('Under-5 vs. Over-5 Total Treamtment Seeking')
 
+row, col = 0, 0
 for key in LAYOUT:
     
     dataset = []
@@ -73,19 +75,26 @@ for key in LAYOUT:
     # Add the data to the plot
     plot = plots[LAYOUT[key][0], LAYOUT[key][1]]
 
+    # Draw the actual plot data
+    plot_data(dataset, dates, plot)
+
     # Format the plot
     plot.set_xlim([min(dates), max(dates)])    
     if LAYOUT[key][1] == 0:
         plot.set_ylim([49, 51])
+        plot.set_ylabel('Mean Treatment Seeking')
     else:
+        plot.set_ylim([76.5, 80])
+        
+    if LAYOUT[key][0] == 1:
         # Draw the seasonality
         for year in range(2018, 2024):
-            plot.axvspan(datetime.datetime(year, 6, 1, 0, 0), datetime.datetime(year, 10, 1, 0, 0), alpha=0.2, color='#CCCCCC')    
+            plot.axvspan(datetime.datetime(year, 6, 1, 0, 0), datetime.datetime(year, 10, 1, 0, 0), alpha=0.2, color='#CCCCCC')            
         
-        plot.set_ylim([76.5, 80])
-
-    # Draw the actual plot data
-    plot_data(dataset, dates, plot)
+    # Format the title
+    result = re.search(r"bfa-s([a-z]*)-(.*).yml", key)
+    plot.title.set_text('S{} {}'.format(result.group(1), result.group(2)))
+    
         
         
     
