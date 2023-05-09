9a5a52e3c0e92fdf5281f8bf53dcbdff21c6f367
rjzupkoii <rjzii@yahoo.com>
2023-05-08 22:09:04 -0400

    Major revisions to the code

--- Analysis/Importation/plot_cellular.py
+++ Analysis/Importation/plot_cellular.py
@@ -10,7 +10,6 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import re
 import sys
 
 # From the PSU-CIDD-MaSim-Support repository
@@ -18,17 +17,10 @@ sys.path.insert(1, '../../../PSU-CIDD-MaSim-Support/Python/include')
 from plotting import scale_luminosity
 
 
-LAYOUT = {
-    'bfa-steady-50-50.yml'   : [0, 0],
-    'bfa-steady-80-20.yml'   : [0, 1],
-    'bfa-seasonal-50-50.yml' : [1, 0],
-    'bfa-seasonal-80-20.yml' : [1, 1]
-    }
-
 STUDYDATE = '2007-01-01'
   
 
-def figure(column, ylabel, filename, ylimit = None):
+def figure(data_file, layout, column, ylabel, filename, ylimit = None):
     # Sub function to render the actual data on the plot
     def plot_data(data, dates, ax):
         # Find the bounds of the data
@@ -43,18 +35,18 @@ def figure(column, ylabel, filename, ylimit = None):
     
 
     # Load the data
-    data = pd.read_csv('data/bfa-cellular.csv')
+    data = pd.read_csv(data_file)
     
     # Prepare the dates in the correct format
     dates = pd.unique(data.dayselapsed)
     startDate = datetime.datetime.strptime(STUDYDATE, "%Y-%m-%d")
-    dates = [startDate + datetime.timedelta(days = int(x)) for x in dates]  
+    dates = [startDate + datetime.timedelta(days = int(x)) for x in dates]
     
     # Setup the figure
     matplotlib.rc_file('include/matplotlibrc-line')
     figure, plots = plt.subplots(2, 2)
     
-    for key in LAYOUT:
+    for key in layout:
         # Filter the data for this plot
         filtered = data[data.filename == key]
         replicates = pd.unique(filtered.id)
@@ -69,13 +61,13 @@ def figure(column, ylabel, filename, ylimit = None):
                 dataset = values
               
         # Add the data to the plot
-        plot = plots[LAYOUT[key][0], LAYOUT[key][1]]
+        plot = plots[layout[key][0], layout[key][1]]
     
         # Draw the actual plot data
         plot_data(dataset, dates, plot)
 
         # Draw the seasonality        
-        if LAYOUT[key][0] == 1:            
+        if layout[key][0] == 1:            
             for year in range(2018, 2038):
                 plot.axvspan(datetime.datetime(year, 6, 1, 0, 0), datetime.datetime(year, 10, 1, 0, 0), alpha=0.2, color='#CCCCCC')            
     
@@ -83,21 +75,71 @@ def figure(column, ylabel, filename, ylimit = None):
         plot.set_xlim([min(dates), max(dates)])
         if ylimit is not None:
             plot.set_ylim(ylimit)
-        if LAYOUT[key][1] == 0:
+        if layout[key][1] == 0:
             plot.set_ylabel(ylabel)
-        result = re.search(r"bfa-s([a-z]*)-(.*).yml", key)
-        plot.title.set_text('S{} {}'.format(result.group(1), result.group(2)))
+        
+        plot.title.set_text(get_title(key))
+        
+    # Set the primary title
+    figure.suptitle(get_title(data_file))
         
     # Save the plot
     plt.savefig(filename, bbox_inches='tight')
-    plt.close()
+    # plt.close()
     
+
+def get_title(filename):
+    MAPPING = {
+        # Grid based studies
+        'bfa-grid-ns-balanced.yml'   : 'No Season, 50-50',
+        'bfa-grid-ns-unbalanced.yml' : 'No Season, 80-20',
+        'bfa-grid-balanced.yml'      : 'Seasonal, 50-50',
+        'bfa-grid-unbalanced.yml'    : 'Seasonal, 80-20',
+        
+        # Single cell studies
+        'bfa-steady-50-50.yml'   : 'No Season, 50-50',
+        'bfa-steady-80-20.yml'   : 'No Season, 80-20',
+        'bfa-seasonal-50-50.yml' : 'Seasonal, 50-50',
+        'bfa-seasonal-80-20.yml' : 'Seasonal, 80-20',
+        
+        # Primary titles
+        'data/bfa-cellular.csv' : 'Single Cell Study',
+        'data/bfa-grid.csv'     : '2x2 Grid Study'
+    }
+    return MAPPING[filename]
+
+
+def plot_cell():
+    FILENAME = 'data/bfa-cellular.csv'
+    LAYOUT = {
+        'bfa-steady-50-50.yml'   : [0, 0],
+        'bfa-steady-80-20.yml'   : [0, 1],
+        'bfa-seasonal-50-50.yml' : [1, 0],
+        'bfa-seasonal-80-20.yml' : [1, 1]
+    }
+
+    figure(FILENAME, LAYOUT, 'clinicalepisodes', 'Total Clinical Episodes', 'out/clinical.png', [3900, 18000])
+    figure(FILENAME, LAYOUT, 'percent_treated', 'Mean Treatment Seeking (%)', 'out/treated.png')
+    figure(FILENAME, LAYOUT, 'frequency', '580Y Frequency', 'out/frequency.png', [0, 0.4])
+    figure(FILENAME, LAYOUT, 'weighted_580y', '580Y Weighted Count', 'out/weighted.png', [0, 60000])
+
+
+def plot_grid():
+    FILENAME = 'data/bfa-grid.csv'
+    LAYOUT = {
+        'bfa-grid-ns-balanced.yml'   : [0, 0],
+        'bfa-grid-ns-unbalanced.yml' : [0, 1],
+        'bfa-grid-balanced.yml'      : [1, 0],
+        'bfa-grid-unbalanced.yml'    : [1, 1]
+    }
+
+    figure(FILENAME, LAYOUT, 'clinicalepisodes', 'Total Clinical Episodes', 'out/grid_clinical.png', [5000, 52000])
+    figure(FILENAME, LAYOUT, 'percent_treated', 'Mean Treatment Seeking (%)', 'out/grid_treated.png')
+    figure(FILENAME, LAYOUT, 'frequency', '580Y Frequency', 'out/grid_frequency.png', [0, 0.25])
+    figure(FILENAME, LAYOUT, 'weighted_580y', '580Y Weighted Count', 'out/grid_weighted.png', [0, 62000])
     
+
 if __name__  == '__main__':
-    figure('clinicalepisodes', 'Total Clinical Episodes', 'out/clinical.png', [3900, 18000])
-    figure('percent_treated', 'Mean Treatment Seeking (%)', 'out/treated.png')
-    figure('frequency', '580Y Frequency', 'out/frequency.png', [0, 0.4])
-    figure('weighted_580y', '580Y Weighted Count', 'out/weighted.png', [0, 60000])
-    
-    
+    plot_cell()
+    plot_grid()
     
